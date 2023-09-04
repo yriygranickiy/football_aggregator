7dd7fd78518d5f3b86bc0e390aad4003d9349c07
yriygranickiy <granickiyrepo@gmail.com>
2023-09-04 15:13:08 +0300

    1) Create class AppConfig for security.

---
+++ src/main/java/com/example/football_aggregator/security/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.football_aggregator.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+}
