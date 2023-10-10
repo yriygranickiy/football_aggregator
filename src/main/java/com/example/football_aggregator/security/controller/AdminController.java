d8c055844faf13de703eb0632c3b69f5e88ad724
yriygranickiy <granickiyrepo@gmail.com>
2023-10-10 14:03:27 +0300

    1) Update classes. 2) Add AdminController

--- src/main/java/com/example/football_aggregator/security/controller/AdminController.java
+++ src/main/java/com/example/football_aggregator/security/controller/AdminController.java
@@ -1,4 +1,12 @@
 package com.example.football_aggregator.security.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/admin")
 public class AdminController {
+
 }
---
+++ src/main/java/com/example/football_aggregator/security/service/AdminService.java
@@ -0,0 +1,16 @@
+package com.example.football_aggregator.security.service;
+
+import com.example.football_aggregator.security.dto.UserDto;
+import com.example.football_aggregator.security.model.Privilege;
+import com.example.football_aggregator.security.model.Role;
+
+public interface AdminService {
+
+    Privilege savePrivilege(Privilege privilegeName);
+
+    Role saveRole(Role roleName);
+
+    UserDto getUserById(Long id);
+
+    UserDto updateUser(UserDto user, Long id);
+}
